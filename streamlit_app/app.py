58a92e64a94c542e22a7cfad63c624ce86ce2b04
UpLong23 <panos-makris@outlook.com>
2026-04-24 20:25:27 +0300

    modified:   streamlit_app/app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -316,7 +316,7 @@ def erp_section(show_grand, cond_checks):
         min_value=-100.0, max_value=100.0,
         # value=float(st.session_state.erp_offset),
         value = 0,
-        step=0.01, format="%.3f",
+        step=0.01, format="%.2f",
         key="erp_offset_input",
     )
     st.session_state.erp_offset = offset
